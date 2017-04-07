fce180a3adf593cbe2fccfea0b7cee33f6b0aef2
twiceYuan <twiceyuan@gmail.com>
2017-04-07 15:44:42 +0800

    生成的抽象类占位方法以异常代替返回默认值

---
+++ core/src/main/java/com/twiceyuan/retrofitmockresult/core/Generated.java
@@ -0,0 +1,15 @@
+package com.twiceyuan.retrofitmockresult.core;
+
+/**
+ * Created by twiceYuan on 2017/4/7.
+ *
+ * 标记生成的类
+ */
+public @interface Generated {
+
+    String value();
+
+    String date();
+
+    String comment() default "";
+}
--- processor/src/main/java/com/twiceyuan/retrofitmockresult/processor/MockApiProcessor.java
+++ processor/src/main/java/com/twiceyuan/retrofitmockresult/processor/MockApiProcessor.java
@@ -1,6 +1,7 @@
 package com.twiceyuan.retrofitmockresult.processor;
 
 import com.google.auto.service.AutoService;
+import com.squareup.javapoet.AnnotationSpec;
 import com.squareup.javapoet.ClassName;
 import com.squareup.javapoet.FieldSpec;
 import com.squareup.javapoet.JavaFile;
@@ -8,11 +9,16 @@ import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeName;
 import com.squareup.javapoet.TypeSpec;
 import com.twiceyuan.retrofitmockresult.core.Constants;
+import com.twiceyuan.retrofitmockresult.core.Generated;
 import com.twiceyuan.retrofitmockresult.core.MockApi;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Locale;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -30,12 +36,16 @@ import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.Elements;
 
-@SuppressWarnings("unused")
 @AutoService(Processor.class)
 public class MockApiProcessor extends AbstractProcessor {
 
     private Filer    filer;
     private Elements elements;
+
+    private SimpleDateFormat mDateFormat = new SimpleDateFormat(
+            "yyyy-MM-dd'T'HH:mm:ss.SSSZ",
+            Locale.getDefault()
+    );
 
     @Override
     public synchronized void init(ProcessingEnvironment processingEnvironment) {
@@ -67,19 +77,12 @@ public class MockApiProcessor extends AbstractProcessor {
         Name qualifiedName = ((TypeElement) mockApi).getQualifiedName();
         String nameString = qualifiedName.toString();
         ClassName className = ClassName.bestGuess(nameString);
-        List<? extends TypeMirror> interfaces = ((TypeElement) mockApi).getInterfaces();
-
-        if (interfaces == null || interfaces.size() != 1) {
-            throw new IllegalStateException("Mock Api 必须实现一个（且只能一个） Api 接口");
-        }
-
-        TypeMirror interfaceClass = interfaces.get(0);
 
         TypeSpec.Builder generateClassBuilder = TypeSpec.classBuilder(className.simpleName() + Constants.SUFFIX)
                 .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
                 .superclass(className);
 
-        appendEmptyMethod(generateClassBuilder, interfaceClass, mockApi);
+        appendEmptyMethod(generateClassBuilder, mockApi);
 
         generateClassBuilder.addField(FieldSpec
                 .builder(boolean.class, "ENABLE",
@@ -88,11 +91,30 @@ public class MockApiProcessor extends AbstractProcessor {
                         Modifier.STATIC)
                 .initializer("$L", isEnable).build());
 
+        generateClassBuilder.addAnnotation(AnnotationSpec.get(new Generated() {
+            @Override
+            public Class<? extends Annotation> annotationType() {
+                return Generated.class;
+            }
+
+            @Override
+            public String value() {
+                return MockApiProcessor.class.getCanonicalName();
+            }
+
+            @Override
+            public String date() {
+                return mDateFormat.format(new Date());
+            }
+
+            @Override
+            public String comment() {
+                return "";
+            }
+        }));
         TypeSpec generateClass = generateClassBuilder.build();
 
-        JavaFile javaFile = JavaFile.builder(className.packageName(), generateClass)
-                .addFileComment("Generated")
-                .build();
+        JavaFile javaFile = JavaFile.builder(className.packageName(), generateClass).build();
 
         try {
             javaFile.writeTo(filer);
@@ -104,49 +126,29 @@ public class MockApiProcessor extends AbstractProcessor {
     /**
      * 给 Mock 实现类动态追加空方法
      */
-    private void appendEmptyMethod(TypeSpec.Builder builder, TypeMirror apiClass, Element mockApiClass) {
+    private void appendEmptyMethod(TypeSpec.Builder builder, Element mockApiClass) {
 
         List<? extends Element> methods = this.elements.getAllMembers((TypeElement) mockApiClass);
 
         for (Element method : methods) {
-            if (method instanceof ExecutableElement) {
-                if (method.getModifiers().contains(Modifier.ABSTRACT)) {
-                    TypeMirror returnType = ((ExecutableElement) method).getReturnType();
-                    MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(method.getSimpleName().toString())
-                            .addAnnotation(Override.class)
-                            .addModifiers(Modifier.PUBLIC)
-                            .returns(TypeName.get(returnType));
-
-                    methodBuilder.addStatement("return" + getDefaultValue(returnType));
-
-                    appendParameters(methodBuilder, (ExecutableElement) method);
-                    builder.addMethod(methodBuilder.build());
-                }
+            if (!(method instanceof ExecutableElement)) {
+                continue;
             }
-        }
-    }
+            if (!method.getModifiers().contains(Modifier.ABSTRACT)) {
+                continue;
+            }
+            TypeMirror returnType = ((ExecutableElement) method).getReturnType();
 
-    /**
-     * 占位方法返回默认值
-     *
-     * @param returnType 根据返回类型返回默认值，避免代码静态检查错误
-     * @return Type 的默认值
-     */
-    private String getDefaultValue(TypeMirror returnType) {
-        String string = returnType.toString();
-        switch (string) {
-            case "void":
-                return "";
-            case "int":
-            case "float":
-            case "double":
-            case "byte":
-            case "long":
-                return " 0";
-            case "boolean":
-                return " false";
-            default:
-                return " null";
+            MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder(method.getSimpleName().toString())
+                    .addAnnotation(Override.class)
+                    .addModifiers(Modifier.PUBLIC)
+                    .returns(TypeName.get(returnType));
+
+            methodBuilder.addStatement("throw new $T($S)", IllegalStateException.class,
+                    method.getSimpleName() + "() is an abstract method!");
+
+            appendParameters(methodBuilder, (ExecutableElement) method);
+            builder.addMethod(methodBuilder.build());
         }
     }
 
